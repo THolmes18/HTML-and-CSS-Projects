b60d780d4adf5d5b0fa1188e9305a9a7c02a520d
THolmes18 <tanasmith96@gmail.com>
2021-09-08 00:00:07 -0600

    Basic_HTML1

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script src="Hello_World!.js">
+</script>
+function My_First_Function() {
+	var String = "Kiss me, I'm Irish!";
+	var result = String.fontcolor("green");
+	document.getElemenetById("Irish").innerHTML =
+	result;
+	}
+	</body>
+</html>
